2ad919c016eebd98fef2b96f375163798d800d9b
Nathan Hazout <nathanh@il.ibm.com>
2015-07-08 10:55:22 +0300

    initial commit

---
+++ InvokingAdapterProceduresSwift/MyConnectListener.swift
@@ -0,0 +1,41 @@
+/*
+*
+COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, modify, and distribute
+these sample programs in any form without payment to IBM® for the purposes of developing, using, marketing or distributing
+application programs conforming to the application programming interface for the operating platform for which the sample code is written.
+Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS AND IBM DISCLAIMS ALL WARRANTIES,
+EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE SAMPLE SOURCE CODE.
+IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
+
+*/
+
+import Foundation
+import IBMMobileFirstPlatformFoundation
+
+class MyConnectListener : NSObject, WLDelegate{
+    let vc : ViewController
+    var strResponse = ""
+    
+    init(vc: ViewController){
+        self.vc = vc
+    }
+    
+    func onSuccess(response: WLResponse!) {
+        var resultText = "Connection success. "
+        if(response != nil){
+            resultText += response.responseText
+        }
+        self.vc.updateView(resultText)
+    }
+    
+    func onFailure(response: WLFailResponse!) {
+        var resultText = "Connection failure. "
+        if(response != nil){
+            resultText += response.errorMsg
+        }
+        self.vc.updateView(resultText)
+    }
+    
+}
